f94de01965fc9d5292ce5f244e90749a2dece663
Girish <girishkumarv21@gmail.com>
2019-08-17 12:17:31 +0530

    second_assignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
-import UserUnput from "./components/UserInput";
-import UserOutput from "./components/UserOutput";
+import ValidationComponent from "./components/ValidationComponent";
+import CharComponent from "./components/CharComponent";
 
 class App extends Component {
+  state = {
+    value: ""
+  };
+
+  valueChangeHandler = event => {
+    this.setState({
+      value: event.target.value
+    });
+  };
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <input
+          type="text"
+          onChange={this.valueChangeHandler}
+          value={this.state.value}
+        />
+        <ValidationComponent length={this.state.value.length} />
+        <CharComponent arr={this.state.value} />
+      </div>
+    );
   }
 }
 
