185e13e474dae3e3b523f4a9d6daced141aae802
Girish <girishkumarv21@gmail.com>
2019-08-17 12:19:34 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
-import ValidationComponent from "./components/ValidationComponent";
-import CharComponent from "./components/CharComponent";
 
 class App extends Component {
-  state = {
-    value: ""
-  };
-
-  valueChangeHandler = event => {
-    this.setState({
-      value: event.target.value
-    });
-  };
-
   render() {
-    return (
-      <div className="App">
-        <input
-          type="text"
-          onChange={this.valueChangeHandler}
-          value={this.state.value}
-        />
-        <ValidationComponent length={this.state.value.length} />
-        <CharComponent arr={this.state.value} />
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
