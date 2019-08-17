67776686789468c49140a846de45ae4a28e05df6
Girish <girishkumarv21@gmail.com>
2019-08-17 09:43:22 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -4,25 +4,8 @@ import UserUnput from "./components/UserInput";
 import UserOutput from "./components/UserOutput";
 
 class App extends Component {
-  state = {
-    userName: ""
-  };
-
-  userInputHandler = event => {
-    this.setState({
-      userName: event.target.value
-    });
-  };
-
   render() {
-    return (
-      <div className="App">
-        <UserUnput change={this.userInputHandler} value={this.state.userName} />
-        <UserOutput name={this.state.userName} />
-        <UserOutput name={this.state.userName} />
-        <UserOutput name={this.state.userName} />
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
