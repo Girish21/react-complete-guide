64fa56d077bc48b07dfdc24a1faf466b5d60c42d
Girish <girishkumarv21@gmail.com>
2019-08-19 20:15:20 +0530

    init

--- src/components/persons/persons.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Person from "./person/person";
-
-const persons = props => (
-  <Person name={props.person.name} change={props.change} delete={props.click} />
-);
-
-export default persons;
