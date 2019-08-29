219cd2a488f49efa947852b53b8789fa834f60bd
Girish <girishkumarv21@gmail.com>
2019-08-29 20:48:29 +0530

    minor change to redux devtools init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import reducer from "./store/reducer/reducer";
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const logger = store => {
+  return next => {
+    return action => {
+      console.log("[Middleware] dispatching action", action);
+      const result = next(action);
+      console.log("[Middleware] updated state", store.getState());
+      return result;
+    };
+  };
+};
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
   <Provider store={store}>
